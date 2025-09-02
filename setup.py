baeeda2bd1af5bc9b379d6439467d233ae0c8020
Huzaifa Ahmed Zaidi <134363832+HuzaifaZaidi@users.noreply.github.com>
2025-09-03 01:09:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,11 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT = "-e ."
-
-
-def get_requirements(file_path: str) -> List[str]:
-    """This function will return the list of requirements"""
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
-    return requirements
-
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.3"
+__version__ = "0.0.5"
 REPO_NAME = "neuron_mlops_mongodb"
 PKG_NAME= "databaseautomationhuz"
 AUTHOR_USER_NAME = "HuzaifaZaidi"
@@ -39,5 +25,4 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requirements("requirements.txt")
     )
